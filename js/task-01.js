54a00b08553eb0d18b52887307a27beb2179dd96
LukaszNiewczas <niewczas1996@gmail.com>
2022-12-10 19:42:13 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,10 @@
 const liItems = document.querySelectorAll("ul#categories li.item");
 
 console.log(`Number of categories: ${liItems.length}`);
+
+const h2s = Array.from(liItems).map((item) => item.querySelector("h2"));
+
+h2s.forEach((h2) => {
+  const listItemNumber = h2.parentElement.querySelectorAll("li").length;
+  console.log(`Category: ${h2.textContent}\nElements: ${listItemNumber}`);
+});
